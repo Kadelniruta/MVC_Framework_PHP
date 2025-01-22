7a44519b9f415314c7460aa3f3d1ce1ec8195173
Kadelniruta <kadelniruta@gmail.com>
2025-01-22 08:41:57 +0545

    commit

--- notes.php
+++ notes.php
@@ -5,10 +5,26 @@ include 'util.php';
 $db = new Database();
 
 
-$statement = $db-> query("SELECT * FROM note");
-$notes = $statement->fetchAll();
+
+// ----Handles note deletion----
+$method = $_POST['_method'] ?? '';
+
+if ($method === 'DELETE') {
+  $id = $_POST['id'];
+
+  $db->query("DELETE FROM note WHERE id = :id", [':id' => $id]);
+
+  header('Location: notes.php');
+}
+// ------------------------------
+
+// ----Fetches notes from the database----
+$notes = $db->query("SELECT * FROM note")->fetchAll(PDO::FETCH_ASSOC);
+
+// ----------------------------------------
 
 
 
 $navTitle = "Note";
 include 'views/notes.view.php';
+
